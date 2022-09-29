799686c763a5a0f8b5fac821d59575c744192d2c
lars20070 <l.nilse@dunelm.org.uk>
2022-09-29 11:27:12 +0200

    strip file extensions

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 Collection of minimal scripts from tutorials and my own tests. Might be useful as templates.
 
 | script | description |
-| --- | --- |
+| :--- | :--- |
 | parallel | example scripts for [GNU Parallel](https://www.gnu.org/software/parallel/) |
--- parallel/script.sh
+++ parallel/script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# generate some jpgs
-rm ./bills/bill_*
-for i in {1..100}
-do
-    echo "$i"
-    cp bills/bill.jpg "bills/bill_$i.jpg"
-done
+# # generate some jpgs
+# rm ./bills/bill_*
+# for i in {1..100}
+# do
+#     echo "$i"
+#     cp bills/bill.jpg "bills/bill_$i.jpg"
+# done
 
 # # convert jpg to png (serial)
 # rm ./bills/*.png
@@ -18,5 +18,18 @@ done
 
 # convert jpg to png (parallel)
 rm ./bills/*.png
-find ./bills -name "*.jpg" | parallel -I% --max-args 1 sips -s format png % --out %.png
-# find ./bills -name "*.jpg" | parallel -I% --max-args 1 echo "${%}%.jpg"
+# find ./bills -name "*.jpg" | parallel -I% --max-args 1 sips -s format png % --out %.png
+find ./bills -name "*.jpg" | parallel --max-args 1 sips -s format png {} --out {.}.png
+
+# # list files
+# find ./bills -name "*" | parallel --max-args 1 echo {}
+# find ./bills -name "*" | parallel --max-args 1 echo {.}     # strip extension
+# find ./bills -name "*" | parallel --max-args 1 echo {/}     # strip path
+# find ./bills -name "*" | parallel --max-args 1 echo {/.}    # strip path and extension
+# find ./bills -name "*" | parallel --max-args 1 echo {//}    # keep only path
+
+# # line-by-line from file
+# # the three lines below are equivalent
+# parallel -a ./bills/bills.txt echo
+# cat ./bills/bills.txt | parallel echo
+# parallel echo < ./bills/bills.txt
