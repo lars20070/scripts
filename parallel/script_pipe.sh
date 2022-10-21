75b59968de53759fc505761104323a3e84eb359b
lars20070 <l.nilse@dunelm.org.uk>
2022-10-21 09:44:20 +0200

    pipe example with block size

--- parallel/script_pipe.sh
+++ parallel/script_pipe.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # simple line + word + characters count
-cat uniprot.fasta | parallel --pipe wc
+# block size 1 MB by default
+< uniprot.fasta parallel --pipe wc
+
+# block size of 5 MB
+< uniprot.fasta parallel --pipe --block 5M wc
