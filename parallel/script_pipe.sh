af1c429cc4639b2d5ed90487e58c2084443d8491
lars20070 <l.nilse@dunelm.org.uk>
2022-10-21 11:07:40 +0200

    test shellcheck

--- parallel/script_pipe.sh
+++ parallel/script_pipe.sh
@@ -5,4 +5,5 @@
 < uniprot.fasta parallel --pipe wc
 
 # block size of 5 MB
-< uniprot.fasta parallel --pipe --block 5M wc
+# < uniprot.fasta parallel --pipe --block 5M wc
+cat uniprot.fasta | parallel --pipe --block 5M wc
