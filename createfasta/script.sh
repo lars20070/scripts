74477c84d8b1f3a89e9c8be314cfa45786f4e738
lars20070 <l.nilse@dunelm.org.uk>
2022-10-21 12:32:39 +0200

    download contaminants

--- createfasta/script.sh
+++ createfasta/script.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-# install ftp
-#brew install inetutils
+# install wget
+# brew install wget
+
+# directory for all ComPIL databases
+databasedir="/Users/lars/Projects/2022/20220628_GenentechComPIL/databases/"
+# home directory
+homedir=$databasedir"test/"
+
+cd $homedir || exit 2
+
+# download contaminants
+# from The Global Proteome Machine (GPM) common Repository of Adventitious Proteins (cRAP)
+# and from MaxQuant
+wget ftp://ftp.thegpm.org/fasta/cRAP/crap.fasta -O contaminants_crap.fasta
+wget "http://lotus1.gwdg.de/mpg/mmbc/maxquant_input.nsf/7994124a4298328fc125748d0048fee2/\$FILE/contaminants.fasta" -O contaminants_maxquant.fasta
